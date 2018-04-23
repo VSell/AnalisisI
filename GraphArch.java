90b89dbebc1aa8c8f1e2e934962b02eebd128aff
VSell <30731717+VSell@users.noreply.github.com>
2018-04-23 00:49:23 -0600

    Add files via upload
    base del grafo

---
+++ GraphArch.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectv1;
+
+/**
+ *
+ * @author Valeria
+ */
+public class GraphArch<T> 
+{
+    private GraphNode _destiny;
+    private int cost;
+
+    public GraphArch() {}
+}
